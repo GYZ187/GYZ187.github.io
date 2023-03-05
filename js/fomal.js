c5850486c2135ebfb9e83d61eeb6d4d89e6c4974
GYZ187 <648473914@qq.com>
2023-03-05 23:07:04 +0800

    Site updated: 2023-03-05 23:07:04

--- js/fomal.js
+++ js/fomal.js
@@ -73,7 +73,7 @@ $.ajax({
   type: 'get',
   url: 'https://apis.map.qq.com/ws/location/v1/ip',
   data: {
-    key: '',  // 这里要写你的KEY!!!
+    key: 'OJ5BZ-74Q6X-V4L43-ZNRZ7-XH5RH-JFFLR',  // 这里要写你的KEY!!!
     output: 'jsonp',
   },
   dataType: 'jsonp',
